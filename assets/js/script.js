143564aa7aaa7d8b00a2a892f6fda8e659c1dfc1
LimaZurmati <urzalalima@gmail.com>
2022-08-20 18:59:51 +0000

    Add event listner for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let button = document.getElementsByTagName("button");
+    for (let button of button){
+        if (this.getAttribute("data-type") === "submit"){
+            alert("You clicked Submiy");
+        } else {
+            let gameType = this.getAttribute("data-type");
+            alert('you clicked $(gameType');
+        }
+    }
+})
 function runGame(){
 
 }
@@ -23,5 +34,5 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+     
 }
