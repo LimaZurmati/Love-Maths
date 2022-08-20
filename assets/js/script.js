4e01df6cdb2cc56c72cb302120364a5a269e8a55
LimaZurmati <urzalalima@gmail.com>
2022-08-20 17:45:46 +0000

    Set upp structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function ncrementWrongAnswer(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
