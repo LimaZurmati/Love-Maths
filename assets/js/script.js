5741583992a31f40693709bd914b1779397d28be
LimaZurmati <urzalalima@gmail.com>
2022-08-22 18:06:47 +0000

    Display addititon to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,71 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let button = document.getElementsByTagName("button");
-    for (let button of button){
-        if (this.getAttribute("data-type") === "submit"){
-            alert("You clicked Submiy");
-        } else {
-            let gameType = this.getAttribute("data-type");
-            alert('you clicked $(gameType');
-            runGame(gameType);
-        }
-    }
-    runGame("addition";)
-})
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-function runGame(gameType){
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    runGame("addition");
+
+});
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-    if (gameType === "addition"){
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
-        alert(`Unkown game type: $(gameType)`);
-        throw `Unknown game type: $(gameType).Aborting!`
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
 }
 
-function checkAnswer(){
+function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function incrementScore(){
+function incrementScore() {
 
 }
 
-function ncrementWrongAnswer(){
-
-}
- function displayAdditionQuestion(){
-    Document.getElementsById('operand1').textContent = operand1;
-    Document.getElementsById('operand2').textContent = operand2;
-    Document.getElementsById('opera').tortextContent = "+";
- }
-function displaySubtractQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displayMultiplyQuestion(){
-     
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
 }
